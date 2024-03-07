54b53c4a34e5f2f11a9f16ec32821aa57ac1da1d
diana pava <dianapava65@gmail.com>
2024-03-07 15:24:31 -0500

    create textview

--- FormPersonalData/FormPagePersonalData/FormPagePersonalDataTextView.swift
+++ FormPersonalData/FormPagePersonalData/FormPagePersonalDataTextView.swift
@@ -2,4 +2,21 @@ import UIKit
 
 class FormPagePersonalDataTextView: UIView {
     
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var dataTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var lineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 }
